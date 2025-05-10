25a4cae6b742da7b35bacfbfe1a73920d57497f5
Nnnathanlethaii <nathanlethai8@gmail.com>
2025-05-09 21:03:25 -0400

    updated ScoreReport

--- pom.xml/src/main/java/com/example/seoanalyzer/ScoreReport.java
+++ pom.xml/src/main/java/com/example/seoanalyzer/ScoreReport.java
@@ -5,4 +5,23 @@ import java.util.List;
 
 public class ScoreReport {
     private int score = 0;
+    private final List<String> details = new ArrayList<>();
+
+    public void addPoints(int pts, String reason){
+        score += pts;
+        details.add(String.format("-%d %s", pts, reason));
+    }
+
+    public void subtractPoints(int pts, String reason){
+        score -= pts;
+        details.add(String.format("-%d %s", pts, reason));
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public List<String> getDetails() {
+        return details;
+    }
 }
